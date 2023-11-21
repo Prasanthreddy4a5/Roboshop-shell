de6651eacf7e4b47cdea7f6bf7ecd058ffb21017
prasanth4a5 <ummadi1994@gmail.com>
2023-11-21 21:32:23 +0530

    Roboshop-shell DNS Records Update

--- user.sh
+++ user.sh
@@ -14,7 +14,7 @@ cd /app
 npm install
 
 dnf install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/user.js
+mongo --host mongodb.prashdevops.online </app/schema/user.js
 
 systemctl daemon-reload
 systemctl enable user
