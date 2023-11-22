d329e7460b33b28977700235fcb26249f7de61ef
prasanth4a5 <ummadi1994@gmail.com>
2023-11-22 21:00:07 +0530

    Echo

--- catalogue.sh
+++ catalogue.sh
@@ -1,41 +1,41 @@
 set -x
 
-echo  ">>>>>>>>>>> Create Catalogue Service <<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>> Create Catalogue Service <<<<<<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
 
-echo  ">>>>>>>>>>> Create Mongo Repo <<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>> Create Mongo Repo <<<<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo  ">>>>>>>>>>> Disable nodejs old module and enable node 18 module<<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>> Disable nodejs old module and enable node 18 module<<<<<<<<<<<<\e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 
-echo  ">>>>>>>>>>> Install NodeJS <<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>> Install NodeJS <<<<<<<<<<<<\e[0m"
 dnf install nodejs -y
 
-echo  ">>>>>>>>>>> Add application User <<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>> Add application User <<<<<<<<<<<<\e[0m"
 useradd roboshop
 
-echo  ">>>>>>>>>>> setup an app directory <<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>> setup an app directory <<<<<<<<<<<<\e[0m"
 mkdir /app
 
-echo  ">>>>>>>>>>> Download the application code to created app directory <<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>> Download the application code to created app directory <<<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
 
-echo  ">>>>>>>>>>> Lets download the dependencies <<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>> Lets download the dependencies <<<<<<<<<<<<\e[0m"
 cd /app
 npm install
 
-echo  ">>>>>>>>>>> install mongodb-client <<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>> install mongodb-client <<<<<<<<<<<<\e[0m"
 dnf install mongodb-org-shell -y
 
-echo  ">>>>>>>>>>> Load Catalogue Schema <<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>> Load Catalogue Schema <<<<<<<<<<<<\e[0m"
 mongo --host mongodb.prashdevops.online </app/schema/catalogue.js
 
 
-echo  ">>>>>>>>>>> Load the Catalogue service <<<<<<<<<<<<<"
+echo -e "\e[36m>>>>>>>>>>> Load the Catalogue service <<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
